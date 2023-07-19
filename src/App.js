6adc05b75abfb3960f0caecc81447bf71b839d1e
Adil Nawaz <adilnawaz256@gmail.com>
2023-07-19 23:37:14 +0530

    Body component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import useFetch from './utils/useFetch'
 import SpinComponent from './Pages/SpinComponent'
+import Body from './components/Body'
+import './App.css'
+
 
 const App = () => {
-    const res = useFetch('https://api.chucknorris.io/jokes/categorie')
+    const res = useFetch('https://api.chucknorris.io/jokes/categories')
     const {data} = res
-    console.log(data);
     if(!data) return <SpinComponent/>
     return(
-        <div>
-
-
+        <div className='Main-container'>
+       <h1 class=" text-4xl text-green-500 animate-bounce font-bold  text-center">Chuck Norries</h1>
+        <div className='flex justify-center flex-wrap'>
+       {
+            Object.values(data).map((ele)=>{
+                return  <Body items={ele}/>
+            })
+       }
+        </div>
         </div>  
     )
   
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Body = () => {
-  return (
-    <div>Body</div>
-  )
+const Body = ({ items }) => {
+    return (
+            <div className='mt-10 m-3 h-full'>
+            <div className=" h-6 w-16 cursor-pointer rounded-md border-black bg-[#FFFFFF] text-center text-lg capitalize text-white shadow-xl hover:border md:h-40 md:w-60 md:p-3">
+                <h1 className="text-sm font-bold capitalize text-blue-900 md:pt-6 md:text-2xl">{items}</h1>
+                <p className="hidden text-sm capitalize text-purple-800 md:block lg:block">unlimited jokes on {items}</p>
+            </div>
+            
+            </div>
+    )
 }
 
 export default Body
